b904199403b0026b8b44d4c07498e633bfbf1941
Hazrat Ali <hazrat8143@gmail.com>
2024-04-20 19:10:39 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -186,7 +186,7 @@ const states = [
       {
         name: "Gaya",
         description:
-          "Gaya is a holy city beside the Falgu River, in the northeast Indian state of Bihar. It’s known for 18th-century Vishnupad Mandir, a riverside temple with an octagonal shrine. Close by, ancient Mangla Gauri Temple is set on a hilltop. ",
+          "Gaya is a holy city beside the Falgu River, in the northeast Indian state of Bihar. It's known for 18th-century Vishnupad Mandir, a riverside temple with an octagonal shrine. Close by, ancient Mangla Gauri Temple is set on a hilltop. ",
         landmarks: [
           {
             name: "Bakraur",
